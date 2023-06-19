6987c7d9602d6bb7f69b50683dfb33f702780326
Kirk <kirk@Tarch.com>
2023-06-19 12:21:31 -0400

    working on gmedit integration

--- talk_codebase/cli.py
+++ talk_codebase/cli.py
@@ -4,23 +4,35 @@ import yaml
 import fire
 import json
 
-from talk_codebase.LLM import factory_llm
-from talk_codebase.consts import DEFAULT_CONFIG
+from LLM import factory_llm
+from consts import DEFAULT_CONFIG
 
 config_path = os.path.join(os.path.expanduser("~"), ".talk_codebase_config.yaml")
 
+def set_config(custom_config_path):
+    global config_path
+    print("config set")
+    config_filename = "talk_codebase_config.yaml"
+    config_path = os.path.join(custom_config_path, config_filename)
+    print(config_path)
+    # Check if config file exists, if not, create an empty one
+    if not os.path.exists(config_path):
+        save_config({})
 
+        
 def get_config():
+    print("get config")
     if os.path.exists(config_path):
         with open(config_path, "r") as f:
             config = yaml.safe_load(f)
     else:
         config = {}
+    print(config)
     return config
 
 
 def save_config(config):
-    home_dir = os.path.expanduser("~")
+    print("saving config")
     with open(config_path, "w") as f:
         yaml.dump(config, f)
 
@@ -54,24 +66,46 @@ def validate_config(config):
     save_config(config)
     return config
 
-
-def chat(root_dir):
-    config = validate_config(get_config())
-    llm = factory_llm(root_dir, config)
-    for line in sys.stdin:
-        query = line.lower().strip()
+def loop(llm):
+    while True:
+        query = input("👉 ").lower().strip()
+        if not query:
+            print("🤖 Please enter a query")
+            continue
         if query in ('exit', 'quit'):
             break
-        response = llm.send_query(query)
-        print(json.dumps(response))
+        llm.send_query(query)
 
+def chat(root_dir, query):
+    print("chatting now")
+    config = validate_config(get_config())
+    llm = factory_llm(root_dir, config)
+    response = llm.send_query(query)
+    print("done chatting for  now")
+    print(json.dumps(response))
+    loop(llm)
 
-def main():
+class TalkCodebaseCLI:
+    def __init__(self):
+        pass
+
+    def configure(self, model_type, api_key=None, model_name=None, model_path=None):
+        configure(model_type, api_key, model_name, model_path)
+
+    def set_config(self, custom_config_path):
+        set_config(custom_config_path)
+
+    def chat(self, root_dir, query):
+        chat(root_dir, query)
+        
+    def set_config_and_chat(self, custom_config_path, root_dir, query):
+        print("running")
+        self.set_config(custom_config_path)
+        self.chat(root_dir, query)
+
+if __name__ == "__main__":
     try:
-        fire.Fire({
-            "chat": chat,
-            "configure": configure
-        })
+        fire.Fire(TalkCodebaseCLI)
     except KeyboardInterrupt:
         print(json.dumps({"status": "exit", "message": "Bye!"}))
     except Exception as e:
@@ -79,7 +113,3 @@ def main():
             print(json.dumps({"status": "error", "message": "Please configure your API key. Use talk-codebase configure"}))
         else:
             raise e
-
-
-if __name__ == "__main__":
-    main()
