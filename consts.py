79fd2e79fc138678649bac29a61d2483c6e15fe1
Kirk <kirk@Tarch.com>
2023-06-24 13:43:34 -0400

    rebuilding code for setup

--- consts.py
+++ consts.py
@@ -1,3 +1,13 @@
+EXCLUDE_DIRS = ['__pycache__', '.venv', '.git', '.idea', 'venv', 'env', 'node_modules', 'dist', 'build', '.vscode',
+                '.github', '.gitlab']
+ALLOW_FILES = ['.txt', '.js', '.mjs', '.ts', '.tsx', '.css', '.scss', '.less', '.html', '.htm', '.json', '.py',
+               '.java', '.c', '.cpp', '.cs', '.go', '.php', '.rb', '.rs', '.swift', '.kt', '.scala', '.m', '.h',
+               '.sh', '.pl', '.pm', '.lua', '.sql','.gml']
+EXCLUDE_FILES = ['requirements.txt', 'package.json', 'package-lock.json', 'yarn.lock']
+MODEL_TYPES = {
+    "OPENAI": "openai",
+    "LOCAL": "local",
+}
 DEFAULT_CONFIG = {
     "api_key":None,
     "max_tokens": "16000",
@@ -11,3 +21,39 @@ DEFAULT_CONFIG = {
     "venv_path":None
 
 }
+
+LOADER_MAPPING = {
+    ".csv": {
+        "loader": CSVLoader,
+        "args": {}
+    },
+    ".doc": {
+        "loader": UnstructuredWordDocumentLoader,
+        "args": {}
+    },
+    ".docx": {
+        "loader": UnstructuredWordDocumentLoader,
+        "args": {}
+    },
+    ".epub": {
+        "loader": UnstructuredEPubLoader,
+        "args": {}
+    },
+    ".md": {
+        "loader": UnstructuredMarkdownLoader,
+        "args": {}
+    },
+    ".pdf": {
+        "loader": PDFMinerLoader,
+        "args": {}
+    }
+}
+
+for ext in ALLOW_FILES:
+    if ext not in LOADER_MAPPING:
+        LOADER_MAPPING[ext] = {
+            "loader": TextLoader,
+            "args": {
+                "encoding": "utf8"
+            }
+        }
